301796ae561265baa3a6f85beacb579e30e45090
jesmigel <jesmigel.developer@gmail.com>
2025-06-03 07:10:46 +1000

    Enable ollama deployment
    - added chart repository and runtime configurations
    - added grafana shell make target

--- tools/make/grafana.mk
+++ tools/make/grafana.mk
@@ -1,8 +1,10 @@
 # MONITORING STACK
-.PHONY: grafana-password grafana-proxy
+.PHONY: grafana-password grafana-proxy grafana-shell
 GRAFANA_NAMESPACE=kube-prometheus-stack
 GRAFANA_PASSWORD=$$(kubectl -n $(GRAFANA_NAMESPACE) get secret $(GRAFANA_NAMESPACE)-grafana -o=jsonpath='{.data.admin-password}' | base64 -d)
 grafana-password:
 	@echo $(GRAFANA_PASSWORD)
+grafana-shell:
+	kubectl -n $(GRAFANA_NAMESPACE) exec -it deploy/$(GRAFANA_NAMESPACE)-grafana -- /bin/sh
 grafana-proxy:
 	kubectl -n $(GRAFANA_NAMESPACE) port-forward svc/$(GRAFANA_NAMESPACE)-grafana 8080:80
