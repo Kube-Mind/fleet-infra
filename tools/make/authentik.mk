48f7ecd42097f21a6f0c5baa4337156b419f5b08
jesmigel <jesmigel.developer@gmail.com>
2025-08-23 18:00:08 +1000

    authentik: add proxy

--- tools/make/authentik.mk
+++ tools/make/authentik.mk
@@ -1,5 +1,5 @@
 .PHONY: authentik-setup authentik-teardown authentik-proxy authentik-password authentik-login authentik-wait
-AUTHENTIK_NAMESPACE=argo-cd
+AUTHENTIK_NAMESPACE=authentik
 AUTHENTIK_PATH=src/platform/security/authentik
 AUTHENTIK_PASSWORD=$$(kubectl -n $(AUTHENTIK_NAMESPACE) get secret authentik-initial-admin-secret -o yaml | grep -o 'password: .*' | sed -e s"/password\: //g" | base64 -d)
 authentik-setup:
@@ -7,3 +7,6 @@ authentik-setup:
 
 authentik-teardown:
 	$(call kustomize_delete,$(AUTHENTIK_PATH))
+
+authentik-proxy:
+	kubectl -n $(AUTHENTIK_NAMESPACE) port-forward svc/authentik-server 8080:80
