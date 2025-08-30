c29cc0905843559c1aae6716795e9da848b98755
jesmigel <jesmigel.developer@gmail.com>
2025-08-31 09:18:05 +1000

    tools: removed legacy targets

--- tools/make/kafka.mk
+++ tools/make/kafka.mk
@@ -1,8 +1,13 @@
 .PHONY: kafka-setup kafka-teardown
-kafka-setup:
 _KAFKA_PATH = src/workloads/messaging/kafka
 
 kafka-setup:
 	$(call kustomize_apply,$(_KAFKA_PATH))
 kafka-teardown:
 	$(call kustomize_delete,$(_KAFKA_PATH))
+
+.PHONY:  kafka-operator-setup kafka-operator-teardown
+kafka-operator-setup:
+	$(call kustomize_apply,$(_KAFKA_PATH)/kafka-operator)
+kafka-operator-teardown:
+	$(call kustomize_delete,$(_KAFKA_PATH)/kafka-operator)
--- tools/make/stable-diffusion.mk
+++ tools/make/stable-diffusion.mk
@@ -1,20 +1,9 @@
-.PHONY: stable-diffusion-port-forward stable-diffusion-logs stable-diffusion-shell
-
-SD_NAMESPACE=stable-diffusion-webui
-stable-diffusion-proxy:
-	kubectl -n $(SD_NAMESPACE) port-forward service/stable-diffusion-webui-git-chart 7860:7860
-
-stable-diffusion-logs:
-	kubectl -n $(SD_NAMESPACE) logs -f deployment.apps/stable-diffusion-webui
-
-stable-diffusion-shell:
-	kubectl -n $(SD_NAMESPACE) exec -ti deployment.apps/stable-diffusion-webui-git-chart -- bash
-
 .PHONY: sd-comfyui-port-forward sd-comfyui-logs sd-comfyui-shell
 
+SD_NAMESPACE=sd-comfyui
 sd-comfyui-port-forward:
-	kubectl -n sd-comfyui port-forward service/sd-comfyui-chart 7860:80
+	kubectl -n $(SD_NAMESPACE) port-forward service/sd-comfyui-chart 7860:80
 sd-comfyui-logs:
-	kubectl -n sd-comfyui logs -f statefulset.apps/sd-comfyui-chart
+	kubectl -n $(SD_NAMESPACE) logs -f statefulset.apps/sd-comfyui-chart
 sd-comfyui-shell:
-	kubectl -n sd-comfyui exec -ti statefulset.apps/sd-comfyui-chart -- bash
+	kubectl -n $(SD_NAMESPACE) exec -ti statefulset.apps/sd-comfyui-chart -- bash
