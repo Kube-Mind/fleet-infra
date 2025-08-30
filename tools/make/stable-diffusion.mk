f37fe5606d99178fe925cf9809556af14d7e9e51
jesmigel <jesmigel.developer@gmail.com>
2025-08-30 19:42:14 +1000

    ai: removed legacy stable-diffusion

--- tools/make/stable-diffusion.mk
+++ tools/make/stable-diffusion.mk
@@ -9,3 +9,12 @@ stable-diffusion-logs:
 
 stable-diffusion-shell:
 	kubectl -n $(SD_NAMESPACE) exec -ti deployment.apps/stable-diffusion-webui-git-chart -- bash
+
+.PHONY: sd-comfyui-port-forward sd-comfyui-logs sd-comfyui-shell
+
+sd-comfyui-port-forward:
+	kubectl -n sd-comfyui port-forward service/sd-comfyui-chart 7860:80
+sd-comfyui-logs:
+	kubectl -n sd-comfyui logs -f statefulset.apps/sd-comfyui-chart
+sd-comfyui-shell:
+	kubectl -n sd-comfyui exec -ti statefulset.apps/sd-comfyui-chart -- bash
